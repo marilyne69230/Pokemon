625c6aeac376d9ba77800fbe5eac530993ba5cfc
Marilyne Naili <marilyne.lydia@gmail.com>
2023-08-30 16:08:28 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// ajoouter toutes les urls
 const routes: Routes = [];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// ajouter les autres component à la suite
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // dépendances externe
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  // la ou on met le composant principal
   bootstrap: [AppComponent]
 })
 export class AppModule { }
