057e17f4aa1ab2fc39afda98f9098012144e57b9
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-01 11:16:36 +0200

    add pokemon list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemons-list';
+import { PokemonClass } from './pokemons';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome Angular {{title}}</h1>
-  <p>{{pokemonList[0]}}</p>`
+  templateUrl: `app.component.html`
 })
 export class AppComponent {
   title: string = 'pokemon-app';
-  pokemonList = ["Pickachu", "Salameche", "Lippoutu"];
-
+  pokemonList: PokemonClass[] = POKEMONS;
+  
+  
   ngOnInit(): void {
     console.log("Je viens de charger")
     // comme console.log mais afficher sous forme de tableau
     console.table(this.pokemonList);
+
+
+    this.selectPokemon(this.pokemonList[0]);
   }
-}
+  
+    selectPokemon(pokemon: PokemonClass): void {
+      // afficher le nom du pokemon
+      console.log("Vous avez sélectionné ce pokémon : " + pokemon['name'])
+
+    }
+
+  }
---
+++ src/app/pokemons.ts
@@ -0,0 +1,9 @@
+export class PokemonClass {
+    id: number;
+    hp: number;
+    cp: number;
+    name: string;
+    picture: string;
+    types: Array<string>;
+    created: Date;
+  }
