3873c603eb71d36dc5ec0e7df67a6e369baa30e0
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-05 09:46:01 +0200

    add error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { Error404Component } from './error404/error404.component';
 
 // ajoouter toutes les urls
 const routes: Routes = [
   // path c apres ip le / et component c le nom du component qu'on a créer
   {path: '', component: HomeComponent},
-  {path: 'article', component: ArticleComponent}
+  {path: 'article', component: ArticleComponent},
+  {path: '**', component: Error404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CardComponent } from './card/card.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error404/error404.component';
 // ajouter les autres component à la suite
 
 @NgModule({
@@ -16,6 +17,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     HomeComponent,
     ArticleComponent,
     NavBarComponent,
+    Error404Component,
   ],
   // dépendances externe
   imports: [
